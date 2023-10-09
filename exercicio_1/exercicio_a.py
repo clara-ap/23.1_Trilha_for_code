7997474d9df240794054afe8d47d22aeaf3abd54
clara-ap <145629102+clara-ap@users.noreply.github.com>
2023-10-09 03:09:13 +0000

    Primeira atualização da questão (a)

---
+++ exercicio_1/exercicio_a.py
@@ -0,0 +1,43 @@
+# pedra, papel e tesoura melhor de 5
+# editar pra ver os resultados a cada rodada e mostrar os pontos no final
+from random import choice
+pontosDoJogador = 0 
+pontosDoBot = 0 
+rodada = 0
+
+while rodada != 5:
+    escolhaDoJogador = input('PEDRA, PAPEL OU TESOURA\nESCOLHA: ')
+    escolhaDoJogador = escolhaDoJogador.lower()
+
+    opcoes = ['pedra','papel','tesoura']
+
+    escolhaDoBot = choice(opcoes)
+
+    if escolhaDoBot == escolhaDoJogador:
+        pontosDoJogador += 1
+        pontosDoBot += 1
+
+    elif escolhaDoBot == 'pedra' and escolhaDoJogador == 'tesoura':
+        pontosDoBot += 1
+
+    elif escolhaDoBot == 'pedra' and escolhaDoJogador == 'papel':
+        pontosDoJogador += 1
+
+    elif escolhaDoBot == 'tesoura' and escolhaDoJogador == 'pedra':
+        pontosDoJogador += 1
+
+    elif escolhaDoBot == 'tesoura' and escolhaDoJogador == 'papel':
+        pontosDoBot += 1
+
+    elif escolhaDoBot == 'papel' and escolhaDoJogador == 'tesoura':
+        pontosDoJogador += 1 
+
+    elif escolhaDoBot == 'papel' and escolhaDoJogador == 'pedra':
+        pontosDoBot += 1
+    
+    rodada += 1
+
+if pontosDoBot > pontosDoJogador:
+    print('VOCÊ PERDEU :(')
+else: 
+    print('VOCÊ GANHOU! ;)')
